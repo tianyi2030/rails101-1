ee3ef2e033c1bb506f166b0bb43b627c0abb1e8a
hy <13950775528@139.com>
2017-05-28 17:53:24 +0800

    implement my group

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,12 @@ Rails.application.routes.draw do
           post :quit
         end
 
-   resources :posts
- end
-   root 'groups#index'
+        resources :posts
+      end
+
+      namespace :account do
+        resources :groups
+      end
+
+    root 'groups#index'
 end
